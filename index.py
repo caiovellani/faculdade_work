bd5a70ceb5ae8611ced83972a469357694e4b508
Caio Vellani <130803251+caiovellani@users.noreply.github.com>
2025-12-04 18:52:50 -0300

    update code

--- index.py
+++ index.py
@@ -1,42 +1,17 @@
+
 import pandas as pd
+df = pd.read_csv('consolidado.xlsx',  delimitert=';' ,  low_memory_ false)
 
-entry_file = "consolidado.xlsx"
-
-exit_file = "consolidado_powerbi.xlsx"
-
-columns = [
-    "HABILIDADES DE TRABALHO",
-    "NODE",
-    "TECNICO",
-    "LOGIN",
-    "SUPERVISOR",
-    "COP",
-    "PERIODO",
-    "INICIO",
-    "FIM",
-    "DESLOCAMENTO",
-    "CONTRATO",
-    "WO",
-    "OS",
-    "COD",
-    "TIPO OS",
-    "JOB COD",
-    "TIPO DE VALOR",
-    "VALOR TECNICO",
-    "PONTO"
+df = df.loc[:, 
+    [
+        "DATA_TOA", 
+        "CIDADES", 
+        "STATUS", 
+        "STATUS SERVICO", 
+        "TIPO DE SERVICO" , 
+        "TIPO RESIDENCIA",
+        "VALOR EMPRESA"
+    ]
 ]
 
-# Carrega a planilha
-df = pd.read_excel(entry_file)
-
-# Mantém apenas as colunas desejadas (colunas que existirem)
-df_filtrado = df[columns]
-
-# Salva o resultado em um novo arquivo
-df_filtrado.to_excel(exit_file, index=False)
-
-print("Arquivo gerado com sucesso:", entry_file)
-
-
-### STEP 2 ###
-df_filtrado = df_filtrado.dropna(subset=["CIDADE"])
+df.dropna(subset=['CIDADES'])
