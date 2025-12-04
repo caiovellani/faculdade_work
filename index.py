5a6b7e1d72e241779c9385357a0a68d8a2936ac6
Caio Vellani <130803251+caiovellani@users.noreply.github.com>
2025-12-04 20:56:24 -0300

    Update index.py

--- index.py
+++ index.py
@@ -1,6 +1,6 @@
 
 import pandas as pd
-df = pd.read_csv('consolidado.xlsx',  delimitert=';' ,  low_memory_ false)
+df = pd.read_csv('D:\Trabalho.csv',  delimitert=';' ,  low_memory=False)
 
 df = df.loc[:, 
     [
@@ -14,6 +14,13 @@ df = df.loc[:,
     ]
 ]
 
-df.dropna(subset=['CIDADES'], inplace=true)
+for i in df.columns:
+    print(f"\n===== (i) =====")
+    print(df[i].value_counts(dropna=False))
 
-df.to_csv('consolidado_limpo.csv', index=False, sep=';')
+df.dropna(subset=['CIDADES'], inplace=True)
+
+df["TIPO RESIDENCIA"] = df["TIPO RESIDENCIA"].replace("Casa", "CASA")
+
+df.to_csv("Trabalho_tratado.csv", index=False, sep=';', encoding="utf-8-sig")
+print("Arquivo CSV salvo com sucesso!") 
